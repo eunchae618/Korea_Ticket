985790e77638d5d34b348e17646586c32cb4c737
eunchae <yonj2sj@naver.com>
2024-07-10 17:47:34 +0900

    feat:일정 티켓 게시판 올려용 ~^^

--- src/pages/gameList.jsx
+++ src/pages/gameList.jsx
@@ -1,11 +1,19 @@
 import React from "react";
+import "./gameList.css";
+import GmImage from "../assets/images/GmImage.png";
 
 const gameList = () => {
     return(
             <div className="league">
                  <h1>K LEAGUE 일정/티켓</h1>
+                 <p> 리그      ㅣ 클럽          ㅣ 예매       </p>
+            
+
+                 <img className="GmImage" src={GmImage} alt="일정 티켓 이미지" />      
             </div>
+            
     );
+
 }
 
 export default gameList;
