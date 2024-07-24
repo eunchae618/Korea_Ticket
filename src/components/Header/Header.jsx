2f5383bf86f202b68c762d428c1801c29c88d7ed
kyuwan <gyuwann@gmail.com>
2024-07-24 16:00:02 +0900

    로그인 기능연결

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,56 @@
 import React, { useState, useRef } from "react";
 import "./Header.css";
-
 import { Link } from "react-router-dom";
+import { postLogin, pacthPwfind, postJoin } from "../../apis/axios.js";
 
 const Header = () => {
   const [modalOpen, setModalOpen] = useState(false);
   const [joinModalOpen, setJoinModalOpen] = useState(false);
   const [passwordModalOpen, setPasswordModalOpen] = useState(false);
   const modalBackground = useRef();
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const [userName, setName] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await postLogin(email, password);
+      alert("로그인 성공");
+      // 로그인 성공 시 추가적인 작업을 수행할 수 있습니다.
+    } catch (error) {
+      setError(error.response ? error.response.data.message : "로그인 실패");
+    }
+  };
+
+  const handleSubmit2 = async (e) => {
+    e.preventDefault();
+    try {
+      await pacthPwfind(email);
+      alert("이메일에 임시 비밀번호 전송");
+      setPasswordModalOpen(false);
+    } catch (error) {
+      setError(
+        error.response ? error.response.data.message : "이메일 확인해주세요."
+      );
+    }
+  };
+
+  const handleSubmit3 = async (e) => {
+    e.preventDefault();
+    try {
+      await postJoin(userName, email, password);
+      alert("회원가입이 완료되었습니다.");
+      setJoinModalOpen(false);
+    } catch (error) {
+      setError(
+        error.response ? error.response.data.message : "아이디가 중복됩니다."
+      );
+    }
+  };
+
   return (
     <>
       <div className="Header">
@@ -26,7 +69,7 @@ const Header = () => {
       </div>
       {modalOpen && (
         <div
-          className={"modal-container"}
+          className="modal-container"
           ref={modalBackground}
           onClick={(e) => {
             if (e.target === modalBackground.current) {
@@ -34,21 +77,32 @@ const Header = () => {
             }
           }}
         >
-          <div className={"modal-content"}>
+          <div className="modal-content">
             <div className="icon">🍀국산티켓</div>
-
-            <form className="login-form">
+            <form className="login-form" onSubmit={handleSubmit}>
               <div className="login-text">
                 <p>이메일</p>
-                <input type="text" placeholder="이메일을 입력해주세요." />
+                <input
+                  type="email"
+                  id="email"
+                  value={email}
+                  placeholder="이메일을 입력해주세요."
+                  onChange={(e) => setEmail(e.target.value)}
+                />
               </div>
               <div className="login-text">
                 <p>비밀번호</p>
-                <input type="password" placeholder="비밀번호를 입력해주세요." />
+                <input
+                  type="password"
+                  value={password}
+                  placeholder="비밀번호를 입력해주세요."
+                  onChange={(e) => setPassword(e.target.value)}
+                />
               </div>
               <div className="modal-button">
-                <button>로그인</button>
+                <button type="submit">로그인</button>
               </div>
+              {error && <div className="error">{error}</div>}
               <div className="modal-menu">
                 <div
                   onClick={() => {
@@ -73,7 +127,7 @@ const Header = () => {
       )}
       {joinModalOpen && (
         <div
-          className={"modal-container"}
+          className="modal-container"
           ref={modalBackground}
           onClick={(e) => {
             if (e.target === modalBackground.current) {
@@ -81,27 +135,40 @@ const Header = () => {
             }
           }}
         >
-          <div className={"modal-content"}>
+          <div className="modal-content">
             <div className="join">🍀회원가입</div>
 
-            <form className="join-form">
+            <form className="join-form" onSubmit={handleSubmit3}>
               <div className="join-text">
                 <p>이름</p>
-                <input type="text" placeholder="ex)홍길동" />
+                <input
+                  type="text"
+                  placeholder="ex)홍길동"
+                  onChange={(e) => setName(e.target.value)}
+                />
               </div>
               <div className="join-text">
                 <p>이메일</p>
-                <input type="text" placeholder="이메일을 입력해주세요." />
+                <input
+                  type="email"
+                  placeholder="이메일을 입력해주세요."
+                  onChange={(e) => setEmail(e.target.value)}
+                />
               </div>
               <div className="join-text">
                 <p>비밀번호</p>
-                <input type="password" placeholder="비밀번호를 입력해주세요." />
+                <input
+                  type="password"
+                  placeholder="비밀번호를 입력해주세요."
+                  onChange={(e) => setPassword(e.target.value)}
+                />
               </div>
               <div className="join-text">
                 <p>비밀번호 확인</p>
                 <input
                   type="password"
                   placeholder="비밀번호를 한번 더 입력해주세요."
+                  onChange={(e) => setPassword(e.target.value)}
                 />
               </div>
               <div className="modal-joinbutton">
@@ -122,7 +189,7 @@ const Header = () => {
       )}
       {passwordModalOpen && (
         <div
-          className={"modal-container"}
+          className="modal-container"
           ref={modalBackground}
           onClick={(e) => {
             if (e.target === modalBackground.current) {
@@ -130,21 +197,19 @@ const Header = () => {
             }
           }}
         >
-          <div className={"modal-content"}>
+          <div className="modal-content">
             <div className="password">🍀비밀번호 찾기</div>
-            <form className="login-form">
-              <div className="pw-text">
-                <p>아이디</p>
-                <input type="text" placeholder="이메일을 입력해주세요." />
-              </div>
-              <div className="pw-text">
-                <p>이름</p>
-                <input type="text" placeholder="이름을 입력해주세요." />
-              </div>
+            <form className="login-form" onSubmit={handleSubmit2}>
               <div className="pw-text">
                 <p>이메일</p>
-                <input type="password" placeholder="이메일을 입력해주세요." />
+                <input
+                  name="email"
+                  type="text"
+                  placeholder="이메일을 입력해주세요."
+                  onChange={(e) => setEmail(e.target.value)}
+                />
               </div>
+
               <div className="modal-backButton">
                 <button>비밀번호 찾기</button>
               </div>
@@ -165,4 +230,4 @@ const Header = () => {
   );
 };
 
-export default Header; //Header 내보내기
+export default Header;
