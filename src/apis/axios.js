582da33a159492cd97b07157b97f77c0dab1a97b
kyuwan <gyuwann@gmail.com>
2024-08-07 13:35:42 +0900

    feat: 국티

--- src/apis/axios.js
+++ src/apis/axios.js
@@ -34,6 +34,18 @@ export const patchRegister = async (email) => {
   }
 };
 
+export const patchRegister4 = async (password, newPassword) => {
+  try {
+    const response = await axiosInstance.patch("/auth/register4", {
+      password,
+      newPassword,
+    });
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
 export const postLogin = async (email, password) => {
   try {
     const response = await axiosInstance.post("/auth/login", {
@@ -79,6 +91,26 @@ export const postJoin = async (userName, email, password) => {
       userName,
       email,
       password,
+    });
+
+    const { accessToken, refreshToken } = response.data;
+
+    // 액세스 토큰은 메모리에 저장 (보안을 위해 localStorage 대신 메모리에 저장하는 것이 권장됩니다)
+    localStorage.setItem("accessToken", accessToken);
+
+    // 리프레시 토큰은 로컬 스토리지에 저장
+    localStorage.setItem("refreshToken", refreshToken);
+    return { accessToken, refreshToken };
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const patchChangePassword = async (password, newPassword) => {
+  try {
+    const response = await axiosInstance.patch("/auth/changePassword", {
+      password: "password",
+      newPassword: "new_password",
     });
 
     const { accessToken, refreshToken } = response.data;
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,13 +1,75 @@
 import React from "react";
 import "./Footer.css";
+import Instagram from "../../assets/images/Instagram.png";
+import Youtube from "../../assets/images/Youtube.png";
+import Tiktok from "../../assets/images/Tictok.png";
+import Twitter from "../../assets/images/Twitter.png";
+import Facebook from "../../assets/images/Facebook.png";
 
 const Footer = () => {
+  const url = "https://www.youtube.com/user/withkleague";
+  const url1 = "https://www.instagram.com/kleague";
+  const url2 = "https://www.facebook.com/KLEAGUEofficial";
+  const url3 = "https://www.tiktok.com/@kleague_official";
+  const url4 = "https://x.com/kleague";
+
   return (
     <footer className="footer-box">
       <div className="footer-text">
-        <p>국산티켓 주식회사</p>
+        <p className="footer-name">국산티켓 주식회사</p>
         <p>대표이사 : 23양은채</p>
         <p>문의사항 : koreaTicket @ gmail.com </p>
+        <p>Copyright ⓒ 23EunChaeYang All Rights Reserved.</p>
+        <div className="Footer-Icon">
+          <div
+            className="Instagram"
+            onClick={() => {
+              window.open(url1);
+            }}
+          >
+            <img
+              className="Instagram-Icon"
+              src={Instagram}
+              alt="인스타그램 아이콘"
+            />
+          </div>
+          <div
+            className="Youtube"
+            onClick={() => {
+              window.open(url);
+            }}
+          >
+            <img className="Youtube-Icon" src={Youtube} alt="유튜브 아이콘" />
+          </div>
+          <div
+            className="Facebook"
+            onClick={() => {
+              window.open(url2);
+            }}
+          >
+            <img
+              className="Facebook-Icon"
+              src={Facebook}
+              alt="페이스북 아이콘"
+            />
+          </div>
+          <div
+            className="Twitter"
+            onClick={() => {
+              window.open(url4);
+            }}
+          >
+            <img className="Twitter-Icon" src={Twitter} alt="트위터 아이콘" />
+          </div>
+          <div
+            className="Tiktok"
+            onClick={() => {
+              window.open(url3);
+            }}
+          >
+            <img className="Tiktok-Icon" src={Tiktok} alt="틱톡 아이콘" />
+          </div>
+        </div>
       </div>
     </footer>
   );
